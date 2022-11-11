56ef30e3344c73d6084b77f6f5f404cb9898ddc3
Raon Choi <h1choi@ucsd.edu>
2022-11-11 15:50:59 -0800

    Update HTML

--- GroupProject/assets/scripts/sidebar.js
+++ GroupProject/assets/scripts/sidebar.js
@@ -1,10 +1,5 @@
 /* functions for sidebar menu open/close */
-function openSide() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeSide() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+function toggleSide() {
+  document.getElementById("mySidebar").classList.toggle("active");
+  document.getElementById("main").classList.toggle("active");
 }
